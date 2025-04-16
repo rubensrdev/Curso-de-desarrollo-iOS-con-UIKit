1045e39d369aba61a743976ae58cc0b13735ba9b
Rubén SR <160135386+rubensrdev@users.noreply.github.com>
2025-04-16 09:50:32 +0200

    Autolayout y constraints intermedio
    Vemos como montar interfaces que se vean bien en vertical y horizontal o cuando cambias de iPhone e iPad mediante los size classes, tanto posición de elementos como variaciones de fuentes

---
+++ Curso completo iOS y Swift con UIKit - Udemy NTT/App3-ConstraintsYAutoLayoutIntermedioUIKit/App3-ConstraintsYAutoLayoutIntermedioUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  App3-ConstraintsYAutoLayoutIntermedioUIKit
+//
+//  Created by Rubén Segura Romo on 16/4/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+

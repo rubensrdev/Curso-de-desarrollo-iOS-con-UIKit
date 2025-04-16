e782d376c55f4602659e4da4f447cf3e052b730a
Rubén SR <160135386+rubensrdev@users.noreply.github.com>
2025-04-16 10:57:31 +0200

    Vistas y controladores de navegación

---
+++ Curso completo iOS y Swift con UIKit - Udemy NTT/App4-VistasYControlesDeNavegacionUIKit/App4-VistasYControlesDeNavegacionUIKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  App4-VistasYControlesDeNavegacionUIKit
+//
+//  Created by Rubén Segura Romo on 16/4/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	@IBOutlet weak var myView: UIView!
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+
+
+}
+
